de452bef323bd70af61e14b83df20de9aaef764b
Choisooyoung98 <ekfms15815@gmail.com>
2022-07-26 18:07:36 +0900

    게시물 객체 3개를 노출

--- src/main/java/com/ll/exam/member/MemberController.java
+++ src/main/java/com/ll/exam/member/MemberController.java
@@ -3,8 +3,7 @@ package com.ll.exam.member;
 import com.ll.exam.Rq;
 
 public class MemberController {
-    public String showLogin(Rq rq) {
+    public void showLogin(Rq rq) {
         rq.appendBody("로그인");
-        return "로그인 폼";
     }
 }
